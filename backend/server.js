983f7d40b35668ee07042a72f58aac83f7dced7a
Braulio <brauliortegabatalla@gmail.com>
2025-06-17 21:31:02 -0500

    extendiendo limite de back avatar

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '2mb' }));
+app.use(express.urlencoded({ extended: true, limit: '2mb' }));
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api', listRoutes);
