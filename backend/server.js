79740010cffd41bfe0511636c13fb3f86a228429
Braulio <brauliortegabatalla@gmail.com>
2025-06-17 21:33:30 -0500

    aumentado limite back

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,8 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json({ limit: '2mb' }));
-app.use(express.urlencoded({ extended: true, limit: '2mb' }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api', listRoutes);
