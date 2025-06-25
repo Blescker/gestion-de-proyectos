2585afa65112e4aec98509ed221351078959dee5
Braulio <brauliortegabatalla@gmail.com>
2025-06-25 18:25:22 -0500

    arreglando rutas 2

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 import http from 'http';
 import {Server} from 'socket.io'
 import path from 'path';
-
+import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.routes.js';
 import projectRoutes from './routes/project.routes.js';
 import listRoutes from './routes/list.routes.js';
@@ -17,7 +17,8 @@ import planificacionRoutes from './routes/planificacion.routes.js';
 import { Message } from './models/Message.js';
 import { Project } from './models/Project.js';
 import { User } from './models/User.js';
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
